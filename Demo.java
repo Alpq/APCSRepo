c0c9336741813ca7f182ef58ff3793f2872e4a52
Alpq <doymazalp@gmail.com>
2020-10-23 11:12:59 -0400

    All functions are working properly

--- Demo.java
+++ Demo.java
@@ -48,19 +48,47 @@ public static String arrayDeepToString(int[][] arr){
   return adderd + "}";
 }
 
-//public static int[][] create2DArray(int rows, int cols, int maxValue){
-//return ;
-//}
+public static int[][] create2DArray(int rows, int cols, int maxValue){
+  int[][] thing = new int[rows][cols];
+  for (int i = 0; i < rows ; i ++)
+  {
+    for (int j = 0; j < cols ; j ++)
+    {
+      thing[i][j] = (int)(Math.random() * maxValue);
+    }
+  }
+  return thing;
+}
 
-//public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue){
-//return 0;
-//}
+public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue){
+  int[] thenumbers = new int[rows];
+  for (int i = 0; i < rows; i ++ )
+  {
+   thenumbers[i] = (int)(Math.random() * cols);
+  }
+  int[][] thing = new int[rows][];
+  for (int i = 0; i < rows; i ++ )
+  {
+   thing[i] = new int[thenumbers[i]];
+  }
+  for (int i = 0; i < rows ; i ++)
+  {
+    for (int j = 0; j < thenumbers[i] ; j ++)
+    {
+      thing[i][j] = (int)(Math.random() * maxValue);
+    }
+  }
+  return thing;
+}
 
   public static void main( String[] args){
     int[] arr = {1, 2, 3};
     int[][] arr2 = { {4, 1, 3, 2}, {4, 0, 4, 0}, {4, 4, 3, 4}, {4, 3, 1, 2}, {0, 0, 0, 0} };
     System.out.println(arrtoString(arr));
     System.out.println(arrayDeepToString(arr2));
+    System.out.println(arrayDeepToString(create2DArray( 4, 5 ,7)));
+    System.out.println(arrayDeepToString(create2DArrayRandomized(4, 5 ,100)));
+
 
   }
 }
