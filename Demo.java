06aa819cab6e835524754bdfe5d816069833d04c
Alpq <doymazalp@gmail.com>
2020-10-23 10:46:22 -0400

    ArrtoString and ArrDeepToString working

--- Demo.java
+++ Demo.java
@@ -1,25 +1,66 @@
 public class Demo{
-  public static void main( String[] args)
+public static void Pyramid(String[] args)
+{
+  int n = 5;
+  if (args.length != 0)
   {
-    int n = 5;
-    if (args.length != 0)
+    try
     {
-      try
-      {
-         n = Integer.parseInt(args[0]);
-      }
-      catch (NumberFormatException e)
-      {
-         n = 5;
-      }
+       n = Integer.parseInt(args[0]);
     }
-    for (int i = 0; i < n; i ++)
+    catch (NumberFormatException e)
     {
-      for (int j = n - i; j > 0; j --)
-      {
-        System.out.print(i + 1);
-      }
-      System.out.println();
+       n = 5;
     }
+  }
+  for (int i = 0; i < n; i ++)
+  {
+    for (int j = n - i; j > 0; j --)
+    {
+      System.out.print(i + 1);
+    }
+    System.out.println();
+  }
+}
+
+public static String arrtoString(int[] arr){
+  String adder = "{";
+  for (int i = 0; i < arr.length; i ++)
+  {
+    adder = adder + Integer.toString(arr[i]);
+    if (i != arr.length - 1)
+    {
+       adder = adder + ", ";
+    }
+  }
+  return adder + "}";
+}
+public static String arrayDeepToString(int[][] arr){
+  String adderd = "{";
+  for (int i = 0; i < arr.length; i ++)
+  {
+    adderd = adderd + arrtoString(arr[i]);
+    if (i != arr.length - 1)
+    {
+      adderd = adderd + ", ";
+    }
+  }
+  return adderd + "}";
+}
+
+//public static int[][] create2DArray(int rows, int cols, int maxValue){
+//return ;
+//}
+
+//public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue){
+//return 0;
+//}
+
+  public static void main( String[] args){
+    int[] arr = {1, 2, 3};
+    int[][] arr2 = { {4, 1, 3, 2}, {4, 0, 4, 0}, {4, 4, 3, 4}, {4, 3, 1, 2}, {0, 0, 0, 0} };
+    System.out.println(arrtoString(arr));
+    System.out.println(arrayDeepToString(arr2));
+
   }
 }
